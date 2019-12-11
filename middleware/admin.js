b8bfa551793e1fcab65a13162872462318d2d924
no@no.com <no@no.com>
2019-12-11 13:45:28 -0500

    wow

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,8 +1,8 @@
 const hasAccessAdmin = (req,res,next)=>
 {
-    if(req.session.userInfo==null || req.session.userInfo.admin == false)
+    if(req.session.userInfo==null || req.session.userInfo.admin == undefined)
     {
-        res.redirect("/user/login");
+        res.redirect("/user/user");
     }
     else
     {
